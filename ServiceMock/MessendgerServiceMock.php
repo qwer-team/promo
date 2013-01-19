67e516b723afdcc3917e1beb25ca6fa8bc4b7ae8
root <root@localhost>
2013-01-19 15:57:20 +0300

    Mock для Storage сервиса.

--- ServiceMock/MessendgerServiceMock.php
+++ ServiceMock/MessendgerServiceMock.php
@@ -1,12 +1,25 @@
 <?php
 
 namespace Qwer\PromoBundle\ServiceMock;
-
+use Symfony\Component\DependencyInjection\ContainerAware;
 /**
  * Description of MessendgerServiceMock
  *
  * @author root
  */
-class MessendgerServiceMock {
-    //put your code here
+class MessendgerServiceMock extends ContainerAware {
+    /**
+     *
+     * $url = $this->get('Messenger')->message($user, array($contact), $subject, $content);
+     *
+     * @param MarketFusion\Bundle\DataBundle\Entity\User $user
+     * @param array ( MarketFusion\Bundle\DataBundle\Entity\UserContact $contact )
+     * @param string $subject
+     * @param string $content
+     * 
+     * @return string Почему строка?
+     */
+    public function message(User $user, $contacts, $subject, $content){
+        return "";
+    }
 }
